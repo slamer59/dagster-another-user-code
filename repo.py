7cf5fd961bb5c43472043c162269083729b54fda
Thomas PEDOT <thomas.pedot@gmail.com>
2021-05-10 18:52:46 +0200

    Another pipeline

--- repo.py
+++ repo.py
@@ -18,27 +18,16 @@ def find_highest_calorie_cereal(_, cereals):
     )
     return sorted_cereals[-1]["name"]
 
-
 @solid
-def find_highest_protein_cereal(_, cereals):
-    sorted_cereals = list(
-        sorted(cereals, key=lambda cereal: cereal["protein"])
-    )
-    return sorted_cereals[-1]["name"]
-
-
-@solid
-def display_results(context, most_calories, most_protein):
+def display_results(context, most_calories):
     context.log.info(f"Most caloric cereal: {most_calories}")
-    context.log.info(f"Most protein-rich cereal: {most_protein}")
 
 
 @pipeline
 def complex_pipeline():
     cereals = download_cereals()
     display_results(
-        most_calories=find_highest_calorie_cereal(cereals),
-        most_protein=find_highest_protein_cereal(cereals),
+        most_calories=find_highest_calorie_cereal(cereals)
     )
 
 @repository
